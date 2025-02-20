680dee741a4bbe7d79adbb5ae77f09503a0aa52d
sindre01 <sindreerichsen@gmail.com>
2025-02-20 14:25:29 +0100

    workflow updated

--- notebooks/few-shot/fox/scripts/push_runs.sh
+++ notebooks/few-shot/fox/scripts/push_runs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 PHASE=${1:-"validation"}
-HOUR=${2:-"0"}
-MINUTES=${3:-"0"}
+H=${2:-"0"}
+M=${3:-"0"}
+S=${4:-"0"}
 TARGET_DIR="notebooks/few-shot/fox/${PHASE}_runs/"
 
 git add "$TARGET_DIR"
-git commit -m "🔄 Updated ${PHASE} runs from HPC FOX at $(date). \n⏱️ Total execution time: ${HOURS}h ${MINUTES}"
+git commit -m "🔄 Updated ${PHASE} runs from HPC FOX at $(date). \n⏱️ Total execution time: ${H}h ${M}m ${S}s"
 git push origin main
