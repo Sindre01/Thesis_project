dd2d5f2fa845b02cb5ab14938dbe6ef0555a45bf
sindre01 <sindreerichsen@gmail.com>
2025-02-23 20:46:40 +0100

    validation with 70b models

--- notebooks/few-shot/fox/run_validation.py
+++ notebooks/few-shot/fox/run_validation.py
@@ -55,16 +55,16 @@ def model_configs(all_responses, model_provider):
     
             models = [
                 # 14b models:
-                "phi4:14b-fp16", #16k context length
-                "qwen2.5:14b-instruct-fp16", #128 k
+                # "phi4:14b-fp16", #16k context length
+                # "qwen2.5:14b-instruct-fp16", #128 k
 
-                #32b models:
-                "qwq:32b-preview-fp16", #ctx: 32,768 tokens
+                # #32b models:
+                # "qwq:32b-preview-fp16", #ctx: 32,768 tokens
                 # "qwen2.5-coder:32b-instruct-fp16", #32,768 tokens
     
                 # #70b models:
-                # "llama3.3:70b-instruct-fp16", #ctx: 130k
-                # "qwen2.5:72b-instruct-fp16", #ctx: 139k
+                "llama3.3:70b-instruct-fp16", #ctx: 130k
+                "qwen2.5:72b-instruct-fp16", #ctx: 139k
             ]
             models_not_tokenized = models_not_in_file(models, f'{project_dir}/notebooks/few-shot/code_max_tokens.json')
             write_models_tokens_to_file(client, models_not_tokenized, all_responses, f'{project_dir}/notebooks/few-shot/code_max_tokens.json')
@@ -214,20 +214,20 @@ if __name__ == "__main__":
             "prompt_type": PromptType.SIGNATURE,
             "semantic_selector": False,
         },
-        {
-            "name": "regular_similarity",
-            "prompt_prefix": "Create a function",
-            "num_shots": [1, 5, 10],
-            "prompt_type": PromptType.REGULAR,
-            "semantic_selector": True,
-        },
-        {
-            "name": "signature_similarity",
-            "prompt_prefix": "Create a function",
-            "num_shots": [1, 5, 10],
-            "prompt_type": PromptType.SIGNATURE,
-            "semantic_selector": True,
-        },
+        # {
+        #     "name": "regular_similarity",
+        #     "prompt_prefix": "Create a function",
+        #     "num_shots": [1, 5, 10],
+        #     "prompt_type": PromptType.REGULAR,
+        #     "semantic_selector": True,
+        # },
+        # {
+        #     "name": "signature_similarity",
+        #     "prompt_prefix": "Create a function",
+        #     "num_shots": [1, 5, 10],
+        #     "prompt_type": PromptType.SIGNATURE,
+        #     "semantic_selector": True,
+        # },
        
     ]
 
