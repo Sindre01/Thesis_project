2a7f17bc45880d1d8df95367a4f075917f94b570
sindre01 <sindreerichsen@gmail.com>
2025-02-24 21:29:25 +0100

    .

--- notebooks/few-shot/fox/submit_job.sh
+++ notebooks/few-shot/fox/submit_job.sh
@@ -11,12 +11,12 @@ EXAMPLES_TYPE="similarity"                 #'coverage' or 'similarity'
 USER="ec-sindrre"                        # Your Educloud username
 HOST="fox.educloud.no"                   # Fox login address (matches SSH config)
 SSH_CONFIG_NAME="fox"                    # Name of the SSH config entry
-ACCOUNT="ec30"                           # Fox project account
-PARTITION="ifi_accel"                        # 'accel' or 'accel_long' (or 'ifi_accel' if access to ec11,ec29,ec30,ec34,ec35 or ec232)
-GPUS=rtx30:4                              # a100 have 40GB or 80GB VRAM, while rtx30 have 24GB VRAM.
+ACCOUNT="ec12"                           # Fox project account
+PARTITION="accel_long"                        # 'accel' or 'accel_long' (or 'ifi_accel' if access to ec11,ec29,ec30,ec34,ec35 or ec232)
+GPUS=a100:2                              # a100 have 40GB or 80GB VRAM, while rtx30 have 24GB VRAM.
 NODES=1                                  # Number of nodes. OLLAMA does currently only support single node inference
 TIME="05-00:00:00"                       # Slurm walltime (D-HH:MM:SS)
-MEM_PER_GPU="20GB"                       # Memory per GPU. 
+MEM_PER_GPU="40G"                       # Memory per GPU. 
 OLLAMA_MODELS_DIR="/cluster/work/projects/ec12/ec-sindrre/ollama-models"  # Path to where the Ollama models are stored and loaded                      
 LOCAL_PORT="11434"                        # Local port for forwarding
 OLLAMA_PORT="11434"                       # Remote port where Ollama listens. If different parallell runs, change ollama_port to avoid conflicts if same node is allocated.
@@ -38,7 +38,7 @@ cat <<EOT > "./scripts/${SBATCH_SCRIPT}"
 ###############################################################################
 
 # Job Configuration
-#SBATCH --job-name=${PHASE}_${EXPERIMENT}         # Job name
+#SBATCH --job-name=${PHASE}_${EXPERIMENT}_${EXAMPLES_TYPE}          # Job name
 #SBATCH --account=${ACCOUNT}                      # Project account
 #SBATCH --partition=${PARTITION}                  # Partition ('accel' or 'accel_long')
 #SBATCH --nodes=${NODES}                           # Amount of nodes. Ollama one support single node inference
