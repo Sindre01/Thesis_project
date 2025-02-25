346f8e6272f936386110659271341311332fa95b
sindre01 <sindreerichsen@gmail.com>
2025-02-25 11:55:45 +0100

    fixes

--- notebooks/few-shot/fox/run_validation.py
+++ notebooks/few-shot/fox/run_validation.py
@@ -63,7 +63,7 @@ def model_configs(all_responses, model_provider):
                 # "qwen2.5-coder:32b-instruct-fp16", #32,768 tokens
     
                 # #70b models:
-                "llama3.3:70b-instruct-fp16", #ctx: 130k
+                # "llama3.3:70b-instruct-fp16", #ctx: 130k
                 "qwen2.5:72b-instruct-fp16", #ctx: 139k
             ]
             models_not_tokenized = models_not_in_file(models, f'{project_dir}/notebooks/few-shot/code_max_tokens.json')
@@ -199,6 +199,7 @@ if __name__ == "__main__":
 
     print("\n==== Running validation ====")
     dataset = read_dataset_to_json(main_dataset_folder + "MBPP-Midio-50.json")
+    experiment_type = "coverage" # "similarity"
     experiments = [
         # { #8 hours on 32b model
         #     "name": "regular_coverage",
@@ -210,7 +211,7 @@ if __name__ == "__main__":
         {
             "name": "signature_coverage",
             "prompt_prefix": "Create a function",
-            "num_shots": [10],
+            "num_shots": [1, 5],
             "prompt_type": PromptType.SIGNATURE,
             "semantic_selector": False,
         },
@@ -262,7 +263,7 @@ if __name__ == "__main__":
     hours, remainder = divmod(int(elapsed_time), 3600)
     minutes, seconds = divmod(remainder, 60)
     print(f"\n⏱️ Total execution time: {hours}h {minutes}m {seconds}s")
-    subprocess.run(["bash", f"{project_dir}/notebooks/few-shot/fox/scripts/push_runs.sh", "validation", str(hours), str(minutes), str(seconds)], check=True)
+    subprocess.run(["bash", f"{project_dir}/notebooks/few-shot/fox/scripts/push_runs.sh", "validation", str(hours), str(minutes), str(seconds), experiment_type ], check=True)
     print("✅ push_runs.sh script executed successfully!")
 
 
