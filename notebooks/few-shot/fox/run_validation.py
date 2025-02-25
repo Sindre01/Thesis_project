32c0beda17e2fc19e72081bcc787eb2c43f09c04
sindre01 <sindreerichsen@gmail.com>
2025-02-25 07:53:57 +0100

    70b

--- notebooks/few-shot/fox/run_validation.py
+++ notebooks/few-shot/fox/run_validation.py
@@ -60,11 +60,11 @@ def model_configs(all_responses, model_provider):
 
                 #32b models:
                 # "qwq:32b-preview-fp16", #ctx: 32,768 tokens
-                "qwen2.5-coder:32b-instruct-fp16", #32,768 tokens
+                # "qwen2.5-coder:32b-instruct-fp16", #32,768 tokens
     
                 # #70b models:
-                # "llama3.3:70b-instruct-fp16", #ctx: 130k
-                # "qwen2.5:72b-instruct-fp16", #ctx: 139k
+                "llama3.3:70b-instruct-fp16", #ctx: 130k
+                "qwen2.5:72b-instruct-fp16", #ctx: 139k
             ]
             models_not_tokenized = models_not_in_file(models, f'{project_dir}/notebooks/few-shot/code_max_tokens.json')
             write_models_tokens_to_file(client, models_not_tokenized, all_responses, f'{project_dir}/notebooks/few-shot/code_max_tokens.json')
