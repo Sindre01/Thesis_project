e2e2b9899301d8648b46f038c3ab9fe4e84ff41e
sindre01 <sindreerichsen@gmail.com>
2025-02-25 14:02:34 +0100

    adds cot

--- notebooks/few-shot/fox/run_validation.py
+++ notebooks/few-shot/fox/run_validation.py
@@ -208,13 +208,13 @@ if __name__ == "__main__":
         #     "prompt_type": PromptType.REGULAR,
         #     "semantic_selector": False,
         # },
-        {
-            "name": "signature_coverage",
-            "prompt_prefix": "Create a function",
-            "num_shots": [1, 5],
-            "prompt_type": PromptType.SIGNATURE,
-            "semantic_selector": False,
-        },
+        # {
+        #     "name": "signature_coverage",
+        #     "prompt_prefix": "Create a function",
+        #     "num_shots": [1, 5, 10],
+        #     "prompt_type": PromptType.SIGNATURE,
+        #     "semantic_selector": False,
+        # },
         # {
         #     "name": "regular_similarity",
         #     "prompt_prefix": "Create a function",
