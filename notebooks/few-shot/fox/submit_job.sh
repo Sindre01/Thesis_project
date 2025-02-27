f6ebacce3de1e0edd22ec1f488fdb2ec35989443
sindre01 <sindreerichsen@gmail.com>
2025-02-27 15:25:11 +0100

    TEST

--- notebooks/few-shot/fox/submit_job.sh
+++ notebooks/few-shot/fox/submit_job.sh
@@ -105,7 +105,7 @@ echo "============= Pulling latest changes from git... ============="
 cd ~/Thesis_project
 
 git status
-
+echo "DEBUG branch: (git rev-parse --abbrev-ref HEAD 2>/dev/null)"
 # Ensure we switch to the correct branch
 if [ "(git rev-parse --abbrev-ref HEAD 2>/dev/null)" != "${PHASE}/${EXAMPLES_TYPE}" ]; then
     echo "❌ Not on ${PHASE}/${EXAMPLES_TYPE}. Committing changes before checkout..."
