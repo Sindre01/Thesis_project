128af36fd6cd8f384eb9b1ed0cfce12a23675f51
sindre01 <sindreerichsen@gmail.com>
2025-02-27 17:27:17 +0100

    chnaged testing

--- notebooks/few-shot/fox/submit_job.sh
+++ notebooks/few-shot/fox/submit_job.sh
@@ -14,7 +14,7 @@ HOST="fox.educloud.no"                   # Fox login address (matches SSH config
 SSH_CONFIG_NAME="fox"                    # Name of the SSH config entry
 ACCOUNT="ec12"                           # Fox project account
 PARTITION="accel"                        # 'accel' or 'accel_long' (or 'ifi_accel' if access to ec11,ec29,ec30,ec34,ec35 or ec232)
-GPUS=4                              # a100 have 40GB or 80GB VRAM, while rtx30 have 24GB VRAM.
+GPUS=3                              # a100 have 40GB or 80GB VRAM, while rtx30 have 24GB VRAM.
 NODES=1                                  # Number of nodes. OLLAMA does currently only support single node inference
 TIME="00-02:00:00"                       # Slurm walltime (D-HH:MM:SS)
 MEM_PER_GPU="40G"                       # Memory per GPU. 
@@ -106,11 +106,11 @@ cd ~/Thesis_project
 
 git status
 
+git pull
 git add ~/Thesis_project/notebooks/${EXPERIMENT}/fox/${PHASE}_runs/
 git commit -m "WIP: Saving work before switching to ${PHASE}/${EXAMPLES_TYPE} branch."
 gut push
 git checkout "${PHASE}/${EXAMPLES_TYPE}"
-
 git pull
 
 source ~/Thesis_project/thesis_venv/bin/activate  # Activate it to ensure the correct Python environment
