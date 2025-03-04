7c16d3dcdb3eb8b20a3fb65f853e496bcda96abe
sindre01 <sindreerichsen@gmail.com>
2025-03-04 11:10:32 +0100

    validation_runs clean

--- notebooks/few-shot/fox/clean_data.py
+++ notebooks/few-shot/fox/clean_data.py
@@ -1,22 +1,25 @@
 import os
 import sys
-
+script_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ''))
 sys.path.append('../../../')
-from my_packages.utils.file_utils import read_dataset_to_json
+from my_packages.utils.file_utils import read_dataset_to_json, write_json_file
 from my_packages.evaluation.code_evaluation import extract_code
-dir = './validation_runs/similarity/signature/'
+dir = f'{script_dir}/validation_runs/coverage/regular/'
+
 for file in os.listdir(dir):
     print(file)
-    data = read_dataset_to_json(dir + file)
+    file_path = dir + file
+    data = read_dataset_to_json(file_path)
+
     for d in data:
         tasks = d["task_candidates"]
         for task_id, code_candiates in tasks.items():
-            for code in code_candiates:
+            for i, code in enumerate(code_candiates):
                 
                 after_clean = extract_code(code)
                 if code != after_clean:
                     print("Removed this part from code: " + code.replace(after_clean, ''))
-                # else:
-                #     print("No change in code")
+                d["task_candidates"][task_id][i] = after_clean
+    write_json_file(file_path, data)
                 
       
