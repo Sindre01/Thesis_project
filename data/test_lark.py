5822908b6e7c8ad186d3c43f095e8c808d3968cd
sindre01 <sindreerichsen@gmail.com>
2025-03-28 10:27:06 +0100

    Syntax

---
+++ data/test_lark.py
@@ -0,0 +1,14 @@
+from lark import Lark
+import sys
+sys.path.append('../')  # Add the path to the my_packages module
+from my_packages.utils.file_utils import read_code_file
+reader = open("midio_grammar.lark", "r")
+your_grammar = reader.read()
+reader.close()
+parser = Lark(your_grammar, parser="lalr", start="start")
+code = read_code_file(1)
+# Parse it
+tree = parser.parse(code)
+
+# Print the raw AST
+print(tree.pretty())
