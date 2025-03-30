9f19ad4fd78e4ba66ca10135b6911406c1a3cb39
sindre01 <sindreerichsen@gmail.com>
2025-03-30 22:44:55 +0200

    chnaged best_params

--- experiments/SynCode/fox/run_validation.py
+++ experiments/SynCode/fox/run_validation.py
@@ -182,7 +182,7 @@ def main(train_data, val_data):
             minutes, seconds = divmod(remainder, 60)
             print(f"\n⏱️ Total execution time: {hours}h {minutes}m {seconds}s")
             subprocess.run(["bash", f"{project_dir}/my_packages/common/push_runs.sh", 
-                            "few-shot", 
+                            experiment_folder, 
                             "validation", 
                             selector_type, 
                             prompt_type,
