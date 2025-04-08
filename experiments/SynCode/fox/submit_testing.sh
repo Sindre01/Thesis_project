1302855d3d6b95b2a1cf12be0d296d6b471edd6f
sindre01 <sindreerichsen@gmail.com>
2025-04-08 17:14:30 +0200

    Refactor identifier regex in midio_grammar.lark for improved clarity; update Slurm scripts to change account, partition, walltime, and memory settings for Ollama API testing.

--- experiments/SynCode/fox/submit_testing.sh
+++ experiments/SynCode/fox/submit_testing.sh
@@ -14,13 +14,13 @@ K_FOLD_JOBS=0-2                              # Runs jobs for folds 0 to 2 (3-fol
 USER="ec-sindrre"                        # Your Educloud username
 HOST="fox.educloud.no"                   # Fox login address (matches SSH config)
 SSH_CONFIG_NAME="fox"                    # Name of the SSH config entry
-ACCOUNT="ec12"                           # Fox project account
-PARTITION="accel"                        # 'accel' or 'accel_long' (or 'ifi_accel' if access to ec11,ec29,ec30,ec34,ec35 or ec232)
+ACCOUNT="ec30"                           # Fox project account
+PARTITION="ifi_accel"                        # 'accel' or 'accel_long' (or 'ifi_accel' if access to ec11,ec29,ec30,ec34,ec35 or ec232)
 GPUS=1                # a100 have 40GB or 80GB VRAM, while rtx30 have 24GB VRAM.
 NODES=1                          # Number of nodes. OLLAMA does currently only support single node inference
 NODE_LIST=    # List of nodes that the job can run on gpu-9,gpu-7,gpu-8,gpu-14
-TIME="1-00:00:00"                  # Slurm walltime (D-HH:MM:SS)
-MEM_PER_GPU="80G"                       # Memory per GPU. 
+TIME="2-00:00:00"                  # Slurm walltime (D-HH:MM:SS)
+MEM_PER_GPU="20G"                       # Memory per GPU. 
 OLLAMA_MODELS_DIR="/cluster/work/projects/ec12/ec-sindrre/ollama-models"  # Path to where the Ollama models are stored and loaded                      
 OLLAMA_PORT="11195"                       # Remote port where Ollama listens. If different parallell runs, change ollama_port to avoid conflicts if same node is allocated.
 SBATCH_SCRIPT="${PHASE}_${EXAMPLES_TYPE}_${PROMPT_TYPE}_${GPUS}_ollama.slurm"           # Slurm batch script name
@@ -51,12 +51,12 @@ experiments='[
 # models='[
 #     "qwq:32b-fp16"
 # ]'
-models='[
-    "phi4:14b-fp16"
-]'
 # models='[
-#     "llama3.2:3b-instruct-fp16"
+#     "phi4:14b-fp16"
 # ]'
+models='[
+    "llama3.2:3b-instruct-fp16"
+]'
 
 # normal* c1-[5-28]
 # accel gpu-[1-2,4-5,7-9,11-13]
