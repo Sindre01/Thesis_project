b3b0a9f7a3081ff1748ca5da6122d58e4dcab1de
sindre01 <sindreerichsen@gmail.com>
2025-04-20 15:13:44 +0200

    Update SYNCODE_CACHE path format and adjust K_FOLD_JOBS and OLLAMA_PORT in submission scripts; modify BACKTICKS_LANG regex in grammar file

--- experiments/SynCode/fox/submit_testing.sh
+++ experiments/SynCode/fox/submit_testing.sh
@@ -10,7 +10,7 @@ PHASE="testing"                       # Phase ('testing' or 'validation')
 EXPERIMENT_TYPE="similarity"                 # 'similarity' or RAG or full-context
 PROMPT_TYPE="signature"                 # 'regular' or 'cot' or 'regular'   
 # SEMANTIC_SELECTOR=true                   # Use semantic selector
-K_FOLD_JOBS=1-2                             # Runs jobs for folds 0 to 2 (3-fold CV)
+K_FOLD_JOBS=0                             # Runs jobs for folds 0 to 2 (3-fold CV)
 USER="ec-sindrre"                        # Your Educloud username
 HOST="fox.educloud.no"                   # Fox login address (matches SSH config)
 SSH_CONFIG_NAME="fox"                    # Name of the SSH config entry
@@ -22,7 +22,7 @@ NODE_LIST=  # List of nodes that the job can run on gpu-14,gpu-9,gpu-7,gpu-8
 TIME="3-00:00:00"                       # Slurm walltime (D-HH:MM:SS)
 MEM_PER_GPU="20G"                       # Memory per GPU. 
 OLLAMA_MODELS_DIR="/cluster/work/projects/ec12/ec-sindrre/ollama-models"  # Path to where the Ollama models are stored and loaded                      
-OLLAMA_PORT="11195"                       # Remote port where Ollama listens. If different parallell runs, change ollama_port to avoid conflicts if same node is allocated.
+OLLAMA_PORT="11095"                       # Remote port where Ollama listens. If different parallell runs, change ollama_port to avoid conflicts if same node is allocated.
 SBATCH_SCRIPT="${PHASE}_${EXPERIMENT_TYPE}_${PROMPT_TYPE}_${GPUS}_ollama.slurm"           # Slurm batch script name
 # Directory on Fox to store scripts and output
 if [ -n "$PROMPT_TYPE" ]; then
