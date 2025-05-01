f3f4d3120c39fe91ec8c2709f7d2e54fc9fca0fb
sindre01 <sindreerichsen@gmail.com>
2025-05-01 10:43:06 +0200

    .

--- my_packages/evaluation/code_evaluation.py
+++ my_packages/evaluation/code_evaluation.py
@@ -478,7 +478,7 @@ def run_model(
         constrained_llm = Syncode(
             model=hf_model, 
             grammar=f"{project_root}/data/midio_grammar.lark", 
-            mode="grammar_mask",
+            mode="original",
             parse_output_only=True, 
             device_map="auto",
             opp=True, # Oppurtinistic or determentisic.
